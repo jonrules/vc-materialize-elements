5690ee1a3ea6f689050c94cbe0c73495e208e7ff
Jonathan <jonrules@gmail.com>
2015-05-28 18:48:25 -0400

    disable unslider autoplay

--- js/unslider-init.js
+++ js/unslider-init.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
 	$('.vc-materialize-elements-unslider').unslider({
 		speed: 500,               //  The speed to animate each slide (in milliseconds)
-		delay: 3000,              //  The delay between slide animations (in milliseconds)
+		delay: false,              //  The delay between slide animations (in milliseconds)
 		complete: function() {},  //  A function that gets called after every slide animation
 		keys: true,               //  Enable keyboard (left, right) arrow shortcuts
 		dots: true,               //  Display dot navigation
